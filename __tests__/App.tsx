2d03f8ac044fade8b13ac85323c928f9fd23ca41
João Alves <njoao97710@gmail.com>
2021-05-29 20:03:32 +0000

    :lipstick: CHORE: rename folder of tests

---
+++ __tests__/App.tsx
@@ -0,0 +1,5 @@
+import {GetServerSideProps} from 'next'
+
+export default function App(): JSX.Element {
+	return <div></div>
+}
--- tests/App.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-
-import { GetServerSideProps } from 'next';
-
-export const App: React.FC = () => <Text />;
